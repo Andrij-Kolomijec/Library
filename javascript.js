ea778a88995f5e76629ac48abfb6da24356a1790
Difficins <a.kolomijec@seznam.cz>
2023-10-08 16:25:06 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book (title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.info = function () {
+
+    }
+
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
