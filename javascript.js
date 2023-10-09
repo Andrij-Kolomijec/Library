c10b971280f4c13a637c8b63c43955e84acfd8b4
Difficins <a.kolomijec@seznam.cz>
2023-10-09 15:16:03 +0200

    New book button

--- javascript.js
+++ javascript.js
@@ -17,16 +17,16 @@ Book.prototype.info = function () {
 
 
 function addBookToLibrary () {
-    let title = prompt('What is the title of the book?');
-    let author = prompt('Who is the author of this book');
-    let pages = prompt('How many pages does this book have?');
-    let read = prompt('Have you read it?');
+    let title = document.querySelector('#submit-title').value;
+    let author = document.querySelector('#submit-author').value;
+    let pages = document.querySelector('#submit-pages').value;
+    let read = document.querySelector('#submit-read').checked;
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-function displayBooks(myLibrary) {
-    for (let item of myLibrary) {
+function displayBook(myLibrary) {
+    for (let item of myLibrary.slice(-1)) {
         const container = document.querySelector('#content');
         const book = document.createElement('div');
         const title = document.createElement('div');
@@ -47,14 +47,55 @@ function displayBooks(myLibrary) {
     }
 }
 
+// New book button
+
 const openNewBookButton = document.querySelector('[data-open-modal]');
 const closeNewBookButton = document.querySelector('[data-close-modal]');
+const submitNewBook = document.querySelector('[data-submit-modal]');
 const modal = document.querySelector('[data-modal]');
+const form = document.querySelector('form');
 
 openNewBookButton.addEventListener('click', () => {
     modal.showModal();
 })
 
-closeNewBookButton.addEventListener('click', () => {
-    modal.close()
+submitNewBook.addEventListener('click', (e) => {
+    e.preventDefault();
+    form.reportValidity(); // shows messages
+    if (form.checkValidity()) { // return true/false if form is valid
+        addBookToLibrary();
+        displayBook(myLibrary);
+        document.querySelector("Form").reset();
+        modal.close();
+    }
 })
+
+form.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        form.reportValidity();
+        if (form.checkValidity()) {
+            addBookToLibrary();
+            displayBook(myLibrary);
+            document.querySelector("Form").reset();
+            modal.close();
+        }
+    }
+})
+
+
+closeNewBookButton.addEventListener('click', () => {
+    modal.close();
+})
+
+modal.addEventListener("click", e => { // to close the window when clicking outside
+    const dialogDimensions = modal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      modal.close()
+    }
+  })
